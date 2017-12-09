828b9f241bbdb666b89dafdb1f09bbad5efdac42
Anders Eliasson <anders.eliasson@digitalroute.com>
2017-12-09 11:45:00 +0100

    desc

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject commentclean "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Clean/remove java-style comments from text"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
